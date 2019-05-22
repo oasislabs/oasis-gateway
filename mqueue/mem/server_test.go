a9b0f4586040f4ac6f57bf04d969d839462da3bd
sharmaster96 <ennsharma@oasislabs.com>
2019-05-22 16:17:17 -0700

    initializing more tests

--- mqueue/mem/server_test.go
+++ mqueue/mem/server_test.go
@@ -5,20 +5,66 @@ import (
 	"testing"
 
 	"github.com/oasislabs/developer-gateway/log"
+	"github.com/oasislabs/developer-gateway/mqueue/core"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestServerRetrieve(t *testing.T) {
+func initializeServer() (*Server, context.CancelFunc) {
 	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
 
 	logger := log.NewLogrus(log.LogrusLoggerProperties{
 		Level: logrus.DebugLevel,
 	})
 	s := NewServer(ctx, logger)
 
+	return s, cancel
+}
+
+func TestServerInsert(t *testing.T) {
+	s, cancel := initializeServer()
+	defer cancel()
+
+	err := s.Insert("key", core.Element{
+		Offset: uint64(1),
+		Value: "value",
+	})
+	assert.Nil(t, err)
+}
+
+func TestServerRetrieve(t *testing.T) {
+	s, cancel := initializeServer()
+	defer cancel()
+
 	els, err := s.Retrieve("key", uint64(1), uint(1))
-	assert.Equal(t, uint64(1), els.Offset)
+	assert.Equal(t, els, core.Elements{
+		Offset: uint64(1),
+		Elements: nil,
+	})
+	assert.Nil(t, err)
+}
+
+func TestServerDiscard(t *testing.T) {
+	s, cancel := initializeServer()
+	defer cancel()
+
+	err := s.Discard("key", uint64(1))
+	assert.Nil(t, err)
+}
+
+func TestServerNext(t *testing.T) {
+	s, cancel := initializeServer()
+	defer cancel()
+
+	offset, err := s.Next("key")
+	assert.Equal(t, uint64(0), offset)
+	assert.Nil(t, err)
+}
+
+func TestServerRemove(t *testing.T) {
+	s, cancel := initializeServer()
+	defer cancel()
+
+	err := s.Remove("key")
 	assert.Nil(t, err)
 }
