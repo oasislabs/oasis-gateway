9ab5843258133068cdd8aeefcd2205e7843b5165
David Yan <david.yan@oasislabs.com>
2019-05-23 17:14:22 -0700

    Add google oauth unit test

---
+++ auth/oauth/google_test.go
@@ -0,0 +1,44 @@
+package oauth
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const CTX_ID_TOKEN = "id_token"
+
+type MockIDToken struct {
+	claims []byte
+}
+
+func (mock *MockIDToken) Claims(v interface{}) error {
+	return json.Unmarshal(mock.claims, v)
+}
+
+type MockIDTokenVerifier struct{}
+
+func (mock *MockIDTokenVerifier) Verify(ctx context.Context, rawIDToken string) (IDToken, error) {
+	return &MockIDToken{claims: []byte(rawIDToken)}, nil
+}
+
+func TestAuthenticateSuccess(t *testing.T) {
+	claims := OpenIDClaims{
+		Email:         "test@email.com",
+		EmailVerified: true,
+	}
+	jsonStr, err := json.Marshal(claims)
+	assert.Nil(t, err)
+
+	req, err := http.NewRequest("POST", "gateway.oasiscloud.io", nil)
+	assert.Nil(t, err)
+	req.Header.Add(ID_TOKEN_KEY, string(jsonStr))
+
+	auth := NewGoogleOauth(&MockIDTokenVerifier{})
+	email, err := auth.Authenticate(req)
+	assert.Nil(t, err)
+	assert.Equal(t, email, "test@email.com")
+}
