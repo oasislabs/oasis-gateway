1baaf95e40559e8a55c786401754726aaed16517
David Yan <david.yan@oasislabs.com>
2019-05-31 15:37:57 -0700

    Add verify test

--- auth/core/verify.go
+++ auth/core/verify.go
@@ -9,6 +9,7 @@ const (
 	cipherLengthOffset = 16
 	aadLengthOffset    = 24
 	cipherOffset       = 32
+	nonceLength        = 5
 )
 
 // Verify the provided AAD in the transaction data with the expected AAD
@@ -19,8 +20,8 @@ func Verify(data string, expectedAAD string) error {
 	cipherLength := binary.BigEndian.Uint64([]byte(data[cipherLengthOffset:aadLengthOffset]))
 	aadLength := binary.BigEndian.Uint64([]byte(data[aadLengthOffset:cipherOffset]))
 
-	if len(data) < int(cipherOffset+cipherLength+aadLength) {
-		return errors.New("Failed to read AAD")
+	if len(data) < int(cipherOffset+cipherLength+aadLength+nonceLength) {
+		return errors.New("Missing data")
 	}
 
 	aadOffset := cipherOffset + cipherLength
