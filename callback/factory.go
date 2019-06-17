0400206e5b3da67a2412bb476622943c853fb66a
eauge <estanislauauge@gmail.com>
2019-06-17 15:32:54 +0200

    add query url to callbacks

--- callback/factory.go
+++ callback/factory.go
@@ -21,9 +21,12 @@ type ClientServices struct {
 // NewClient creates a new instance of the client with the
 // specified configuration and the provided services
 func NewClient(ctx context.Context, services *ClientServices, config *Config) (*client.Client, error) {
-	var bodyFormat *template.Template
+	var (
+		bodyFormat     *template.Template
+		queryURLFormat *template.Template
+	)
 	if len(config.WalletOutOfFunds.Body) > 0 {
-		tmpl, err := template.New("WalletOutOfFunds").Parse(config.WalletOutOfFunds.Body)
+		tmpl, err := template.New("WalletOutOfFundsBody").Parse(config.WalletOutOfFunds.Body)
 		if err != nil {
 			return nil, err
 		}
@@ -31,18 +34,28 @@ func NewClient(ctx context.Context, services *ClientServices, config *Config) (*
 		bodyFormat = tmpl
 	}
 
+	if len(config.WalletOutOfFunds.QueryURL) > 0 {
+		tmpl, err := template.New("WalletOutOfFundsQueryURL").Parse(config.WalletOutOfFunds.QueryURL)
+		if err != nil {
+			return nil, err
+		}
+
+		queryURLFormat = tmpl
+	}
+
 	return client.NewClient(&client.Services{
 		Logger: services.Logger,
 	}, &client.Props{
 		Callbacks: client.Callbacks{
 			WalletOutOfFunds: client.Callback{
-				Enabled:     config.WalletOutOfFunds.Enabled,
-				Name:        "WalletOutOfFunds",
-				Method:      config.WalletOutOfFunds.Method,
-				URL:         config.WalletOutOfFunds.URL,
-				BodyFormat:  bodyFormat,
-				Headers:     config.WalletOutOfFunds.Headers,
-				PeriodLimit: 1 * time.Minute,
+				Enabled:        config.WalletOutOfFunds.Enabled,
+				Name:           "WalletOutOfFunds",
+				Method:         config.WalletOutOfFunds.Method,
+				URL:            config.WalletOutOfFunds.URL,
+				BodyFormat:     bodyFormat,
+				QueryURLFormat: queryURLFormat,
+				Headers:        config.WalletOutOfFunds.Headers,
+				PeriodLimit:    1 * time.Minute,
 			},
 		},
 	}), nil
