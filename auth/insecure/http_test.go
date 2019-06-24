18b30e36a1853d99268f5ab6f89a6ca456ff2a6f
Will Scott <will@oasislabs.com>
2019-06-24 06:42:13 -0700

    refactor circular test dep

--- auth/insecure/http_test.go
+++ auth/insecure/http_test.go
@@ -1,10 +1,10 @@
-package core
+package insecure
 
 import (
 	"net/http"
 	"testing"
 
-	"github.com/oasislabs/developer-gateway/auth/insecure"
+	"github.com/oasislabs/developer-gateway/auth/core"
 	"github.com/oasislabs/developer-gateway/log"
 	"github.com/oasislabs/developer-gateway/rpc"
 	"github.com/stretchr/testify/assert"
@@ -13,37 +13,37 @@ import (
 type MockHTTPMiddleware struct{}
 
 func (h *MockHTTPMiddleware) ServeHTTP(req *http.Request) (interface{}, error) {
-	return req.Context().Value(ContextAuthDataKey), nil
+	return req.Context().Value(core.ContextAuthDataKey), nil
 }
 
 func TestServeHTTP(t *testing.T) {
-	httpMiddlewareAuth := NewHttpMiddlewareAuth(
-		insecure.InsecureAuth{},
+	httpMiddlewareAuth := core.NewHttpMiddlewareAuth(
+		InsecureAuth{},
 		log.NewLogrus(log.LogrusLoggerProperties{}),
 		&MockHTTPMiddleware{})
 
 	req, err := http.NewRequest("POST", "gateway.oasiscloud.io", nil)
 	assert.Nil(t, err)
-	req.Header.Add(insecure.HeaderKey, "insecure-key")
-	req.Header.Add(RequestHeaderSessionKey, "session-key")
+	req.Header.Add(HeaderKey, "insecure-key")
+	req.Header.Add(core.RequestHeaderSessionKey, "session-key")
 
 	response, err := httpMiddlewareAuth.ServeHTTP(req)
 	assert.Nil(t, err)
-	authData := response.(AuthData)
+	authData := response.(core.AuthData)
 	assert.Equal(t, "017fdef9eeec58e0ad6b94721a2eb52a9bd96dddd9aa2f1e058153568f4ed42d:session-key", authData.SessionKey)
 	assert.Equal(t, "insecure-key", authData.ExpectedAAD)
 	assert.NotNil(t, authData.SessionKey)
 }
 
 func TestServeHTTPInvalidSessionKey(t *testing.T) {
-	httpMiddlewareAuth := NewHttpMiddlewareAuth(
-		insecure.InsecureAuth{},
+	httpMiddlewareAuth := core.NewHttpMiddlewareAuth(
+		InsecureAuth{},
 		log.NewLogrus(log.LogrusLoggerProperties{}),
 		&MockHTTPMiddleware{})
 
 	req, err := http.NewRequest("POST", "gateway.oasiscloud.io", nil)
 	assert.Nil(t, err)
-	req.Header.Add(insecure.HeaderKey, "insecure-key")
+	req.Header.Add(HeaderKey, "insecure-key")
 
 	response, err := httpMiddlewareAuth.ServeHTTP(req)
 	assert.NotNil(t, err)
@@ -52,28 +52,28 @@ func TestServeHTTPInvalidSessionKey(t *testing.T) {
 }
 
 func TestServeHTTPNonMatchingSessionKeys(t *testing.T) {
-	httpMiddlewareAuth := NewHttpMiddlewareAuth(
-		insecure.InsecureAuth{},
+	httpMiddlewareAuth := core.NewHttpMiddlewareAuth(
+		InsecureAuth{},
 		log.NewLogrus(log.LogrusLoggerProperties{}),
 		&MockHTTPMiddleware{})
 
 	req1, err := http.NewRequest("POST", "gateway.oasiscloud.io", nil)
 	assert.Nil(t, err)
-	req1.Header.Add(insecure.HeaderKey, "user-1")
-	req1.Header.Add(RequestHeaderSessionKey, "session-key")
+	req1.Header.Add(HeaderKey, "user-1")
+	req1.Header.Add(core.RequestHeaderSessionKey, "session-key")
 
 	response1, err := httpMiddlewareAuth.ServeHTTP(req1)
 	assert.Nil(t, err)
-	authData1 := response1.(AuthData)
+	authData1 := response1.(core.AuthData)
 
 	req2, err := http.NewRequest("POST", "gateway.oasiscloud.io", nil)
 	assert.Nil(t, err)
-	req2.Header.Add(insecure.HeaderKey, "user-2")
-	req2.Header.Add(RequestHeaderSessionKey, "session-key")
+	req2.Header.Add(HeaderKey, "user-2")
+	req2.Header.Add(core.RequestHeaderSessionKey, "session-key")
 
 	response2, err := httpMiddlewareAuth.ServeHTTP(req2)
 	assert.Nil(t, err)
-	authData2 := response2.(AuthData)
+	authData2 := response2.(core.AuthData)
 
 	assert.NotEqual(t, authData1.SessionKey, authData2.SessionKey)
 }
