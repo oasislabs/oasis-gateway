9c6f3ceecea37f74d5c5f8729aabbf9964c40bbf
eauge <estanislauauge@gmail.com>
2019-09-03 14:18:18 -0700

    use goproxy for docker build image

--- .buildkite/scripts/docker_update_build_image.sh
+++ .buildkite/scripts/docker_update_build_image.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 echo "---- Build and run unit tests"
 
-docker build -t oasislabs/developer-gateway:build -f .buildkite/Dockerfile.ci .
+docker build --build-arg GOPROXY="$GOPROXY" -t oasislabs/developer-gateway:build -f .buildkite/Dockerfile.ci .
 docker push oasislabs/developer-gateway:build
 
 exit 0
